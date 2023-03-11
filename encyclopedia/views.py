7629d69b48a4cdedfdc0e6e9efd76a9991522fac
kenshin009 <movie9time9@gmail.com>
2023-03-11 17:56:24 +0630

    final testing complete

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,11 @@
 from urllib import request
+import random
+import markdown2
+from . import util
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-
-
-from . import util
 from django.urls import reverse
-import markdown2
-
-import random
 from random import choice
-
 
 
 def index(request):
@@ -21,7 +17,7 @@ def entry(request,title):
             entry_content = util.get_entry(title)
             if entry_content == None:
                 return render(request,'encyclopedia/error.html',{
-                    'error': 'This entry does not exist',
+                    'error': 'The requested page was not found',
                 })
             else:
                 mark_content = markdown2.markdown(entry_content)
@@ -33,12 +29,22 @@ def entry(request,title):
 
 def search(request):
         if request.method == 'POST':
+            # Get the words searched by user
             q = request.POST['q']
+            # Get the list of all entry names
             entries = util.list_entries()
+            # Change the entry names to upper case
+            entries_upper = []
+            for entry in entries:
+              entry = entry.upper()
+              entries_upper.append(entry)
+
             results = []
-            if q in entries:
+            # Check if the searched entry name is already in entry
+            if q.upper() in entries_upper:
                return HttpResponseRedirect(reverse('encyclopedia:entry',args=(q,)))
             else:
+                # Filter the entry names which contains search-words
                 for entry in entries:
                     if q.upper() in entry.upper():
                         results.append(entry)
@@ -57,7 +63,7 @@ def edit(request,title):
             title = request.POST['title']
             content = request.POST['content']
             util.save_entry(title,content)
-        return HttpResponseRedirect(reverse('encyclopedia:entry',args=[title]))
+        return HttpResponseRedirect(reverse('encyclopedia:entry',args=(title,)))
 
 def new(request):
     if request.method == 'GET':
@@ -68,7 +74,7 @@ def new(request):
         exist = util.get_entry(title)
         if exist is None:
            util.save_entry(title,content)
-           return HttpResponseRedirect(reverse('encyclopedia:entry',args=[title]))
+           return HttpResponseRedirect(reverse('encyclopedia:entry',args=(title,)))
         else:
             return render(request,'encyclopedia/error.html',{
                 'message': 'Enry page already exists.',
--- entries/Coffee.md
+++ entries/Coffee.md
@@ -4,4 +4,10 @@
 
 
 
-Coffee is my favorite drink. Mostly, I drink [Americano](https://en.wikipedia.org/wiki/Caff%C3%A8_Americano) and Cappuccino.
+
+
+
+
+
+
+Coffee is my favorite drink. Mostly, I drink [Americano](https://en.wikipedia.org/wiki/Caff%C3%A8_Americano) and Latte.
